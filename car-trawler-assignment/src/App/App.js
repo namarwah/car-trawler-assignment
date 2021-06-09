9ffc83e88aa8e81516fd0e6781a7fd44d0068ded
namarwah <marwahnakuul@gmail.com>
2021-06-09 15:36:59 +0100

    [WIP] Initial Project Setup

---
+++ car-trawler-assignment/src/App/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Initial Setup</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
